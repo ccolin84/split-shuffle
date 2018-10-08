5ff9e0a64eb893cb7e4c373e6e058c4b4524ac96
ccolin84 <ccolin84@gmail.com>
2018-10-08 17:42:10 -0400

    break shuffle template fn into its own header file

---
+++ src/shuffle.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <ctime>
+
+namespace shuffle
+{
+template <typename T>
+void inplace_fish_yates_shuffle(std::vector<T> &numbers_to_shuffle)
+{
+  srand(time(0));
+  for (size_t i = numbers_to_shuffle.size() - 1; i > 0; i--)
+  {
+    unsigned int rand_inx = rand() % i;
+    std::iter_swap(std::begin(numbers_to_shuffle) + i, std::begin(numbers_to_shuffle) + rand_inx);
+  }
+}
+}
