a7f5ce78055c875b21adee3382fe99e93d17e1b3
ccolin84 <ccolin84@gmail.com>
2018-10-08 20:25:23 -0400

    add build script for tests

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,3 +1,4 @@
 sh build/build_create_sample_input.sh
 sh build/build_split_shuffle.sh
 sh build/build_run_split_shuffle.sh
+sh build/build_tests.sh
---
+++ build/build_tests.sh
@@ -0,0 +1,6 @@
+g++ \
+  tests/split-shuffle-test.cpp \
+  src/split-shuffle.cpp \
+  -o bin/tests \
+  -I src/ \
+  -std=c++17
