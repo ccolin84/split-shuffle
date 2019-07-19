67f4706632c6065cbbc0d964d4d91eb9d1716683
colinlcrawford <ccolin84@gmail.com>
2019-07-19 08:44:45 -0400

    add a basic unit test for fisher yates shuffle

---
+++ unit-tests/shuffle.spec.h
@@ -0,0 +1,28 @@
+#include "lib/shuffle.h"
+
+void shuffle_spec()
+{
+  test::TestGroup("shuffle", []() {
+    test::TestCase(
+        "inplace_fisher_yates_shuffle",
+        "should shuffle an input vector",
+        []() {
+            int input_vector_size = 20;
+            auto input = std::vector<int>();
+            input.reserve(input_vector_size);
+            for (int i = 0; i < input_vector_size; i++)
+            {
+                input.push_back(i);
+            }
+            auto shuffled_input = std::vector(input);
+
+            shuffle::inplace_fisher_yates_shuffle(shuffled_input);
+
+            if (input == shuffled_input)
+            {
+                return test::TestResult{true, "Failed!"};
+            }
+            return test::TestResult{false};
+        }).run();
+  }).run();
+}
